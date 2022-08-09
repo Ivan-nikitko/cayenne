f676ed4f45c4f6b62162899927c3bee7aba0dd28
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2022-08-09 16:04:25 +0200

    del unused class and methods

--- modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/PanelFactory.java
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/util/PanelFactory.java
@@ -20,24 +20,16 @@
 
 package org.apache.cayenne.modeler.util;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
-
-import com.jgoodies.forms.builder.DefaultFormBuilder;
-import com.jgoodies.forms.layout.FormLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /** 
  * Implements a set of utility methods for laying out components on the panels.
@@ -60,60 +52,6 @@ public class PanelFactory {
         }
 
         return panel;
-    }
-
-    public static JPanel createForm(
-        String title,
-        String[] labels,
-        Component[] components) {
-        Component[] jlabels = new Component[labels.length];
-        for (int i = 0; i < labels.length; i++) {
-            jlabels[i] = new JLabel(labels[i]);
-        }
-        return createForm(title, jlabels, components);
-    }
-
-    public static JPanel createForm(
-        Component[] leftComponents,
-        Component[] rightComponents) {
-        return createForm(null, leftComponents, rightComponents);
-    }
-
-    /** 
-     * Create panel with aligned labels on the right and fields on the left.
-     */
-    public static JPanel createForm(
-        String title,
-        Component[] leftComponents,
-        Component[] rightComponents) {
-
-        if (leftComponents.length != rightComponents.length) {
-            throw new IllegalArgumentException(
-                "Arrays must be the same size, instead got "
-                    + leftComponents.length
-                    + "and "
-                    + rightComponents.length);
-        }
-
-        int numRows = leftComponents.length;
-        if (numRows == 0) {
-            throw new IllegalArgumentException("Zero components.");
-        }
-
-        FormLayout layout = new FormLayout("right:100, 3dlu, left:300", "");
-        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
-        builder.setDefaultDialogBorder();
-
-        if (title != null) {
-            builder.appendSeparator(title);
-        }
-
-        for (int i = 0; i < numRows; i++) {
-            builder.append(leftComponents[i], rightComponents[i]);
-            builder.nextLine();
-        }
-
-        return builder.getPanel();
     }
 
     /** 
@@ -141,39 +79,6 @@ public class PanelFactory {
         panel.add(scrollPane, BorderLayout.CENTER);
 
         // Add Add and Remove buttons
-        if (buttons != null) {
-            panel.add(createButtonPanel(buttons), BorderLayout.SOUTH);
-        }
-        return panel;
-    }
-
-    /** Creates panel with table within scroll panel and buttons in the bottom.
-      * Also sets the resizing and selection policies of the table to
-      * AUTO_RESIZE_OFF and SINGLE_SELECTION respectively.*/
-    public static JPanel createTablePanel(
-        JTable table,
-        JComponent[] components,
-        JButton[] buttons) {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout(5, 5));
-
-        JPanel temp_panel = new JPanel(new BorderLayout());
-
-        // Create table with two columns and no rows.
-        table.getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        JScrollPane scroll_pane = new JScrollPane(table);
-        temp_panel.add(scroll_pane, BorderLayout.CENTER);
-
-        for (JComponent component : components) {
-            JPanel temp = new JPanel(new BorderLayout());
-            temp.add(temp_panel, BorderLayout.CENTER);
-            temp.add(component, BorderLayout.SOUTH);
-            temp_panel = temp;
-        }
-
-        panel.add(temp_panel, BorderLayout.CENTER);
-
         if (buttons != null) {
             panel.add(createButtonPanel(buttons), BorderLayout.SOUTH);
         }
